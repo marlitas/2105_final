42d44d2e6e4648af479efd66584222c59c4bc30a
Marla Schulz <marlaa.schulz@gmail.com>
2021-06-16 10:22:38 -0600

    Complete iteration 3

--- lib/train_yard.rb
+++ lib/train_yard.rb
@@ -20,4 +20,30 @@ class TrainYard
       train.cargo.keys.include?(car)
     end
   end
+
+  def sorted_cargo_list
+    @trains.flat_map do |train|
+      train.cargo.map do |car, amount|
+        car.type
+      end
+    end.sort.uniq
+  end
+
+  def total_inventory
+    hash = Hash.new(0)
+    @trains.each do |train|
+      train.cargo.each do |car, amount|
+        hash[car] = hash[car] + train.count_cars(car)
+      end
+    end
+    hash
+  end
+
+  def overflow_cars
+    total_inventory.find_all do |car, amount|
+      (amount > 10) && (trains_containing(car).length > 1)
+    end.flat_map do |array|
+      array.first
+    end
+  end
 end
