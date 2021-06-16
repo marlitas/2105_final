8521d8f1dd3b3702880f1eb032a128a090be7dae
Marla Schulz <marlaa.schulz@gmail.com>
2021-06-16 09:49:37 -0600

    Add more method testing

--- spec/train_yard_spec.rb
+++ spec/train_yard_spec.rb
@@ -15,22 +15,6 @@ RSpec.describe TrainYard do
     @train2 = Train.new({name: 'Emily', type: 'Tender'})
     @train3 = Train.new({name: 'Nia', type: 'Tank'})
     @train4 = Train.new({name: 'Gordon', type: 'Express'})
-
-    @train1.add_cars(@car1, 5)
-    @train1.add_cars(@car2, 2)
-
-    @train2.add_cars(@car1, 3)
-    @train2.add_cars(@car4, 4)
-
-    @train3.add_cars(@car1, 1)
-
-    @train4.add_cars(@car4, 5)
-    @train4.add_cars(@car3, 10)
-
-    @train_yard.add_train(@train1)
-    @train_yard.add_train(@train2)
-    @train_yard.add_train(@train3)
-    @train_yard.add_train(@train4)
   end
 
   it 'exists' do
@@ -39,17 +23,69 @@ RSpec.describe TrainYard do
 
   it 'has attributes' do
     expect(@train_yard.location).to eq('Brighton')
+    expect(@train_yard.trains).to eq([])
   end
 
-  it 'can add trains' do
-    expect(@train_yard.trains).to eq([@train1, @train2, @train3, @train4])
+  context 'methods' do
+    before(:each) do
+      @train1.add_cars(@car1, 5)
+      @train1.add_cars(@car2, 2)
+
+      @train2.add_cars(@car1, 3)
+      @train2.add_cars(@car4, 4)
+
+      @train3.add_cars(@car1, 1)
+
+      @train4.add_cars(@car4, 5)
+      @train4.add_cars(@car3, 10)
+
+      @train_yard.add_train(@train1)
+      @train_yard.add_train(@train2)
+      @train_yard.add_train(@train3)
+      @train_yard.add_train(@train4)
+    end
+
+    it 'can add trains' do
+      expect(@train_yard.trains).to eq([@train1, @train2, @train3, @train4])
+    end
+
+    it 'can list types of trains' do
+      expect(@train_yard.types_of_trains).to eq(['Express', 'Tank', 'Tender'])
+    end
+
+    it 'can list trains containing specific car type' do
+      expect(@train_yard.trains_containing(@car1)).to eq([@train1, @train2, @train3])
+    end
   end
 
-  it 'can list types of trains' do
-    expect(@train_yard.types_of_trains).to eq(['Express', 'Tank', 'Tender'])
-  end
+  context 'more methods' do
+    before(:each) do
+      @train1.add_cars(@car1, 5)
 
-  it 'can list trains containing specific car type' do
-    expect(@train_yard.trains_containing(@car1)).to eq([@train1, @train2, @train3])
+      @train2.add_cars(@car1, 3)
+      @train2.add_cars(@car4, 4)
+
+      @train3.add_cars(@car1, 4)
+
+      @train4.add_cars(@car4, 5)
+      @train4.add_cars(@car3, 10)
+
+      @train_yard.add_train(@train1)
+      @train_yard.add_train(@train2)
+      @train_yard.add_train(@train3)
+      @train_yard.add_train(@train4)
+    end
+
+    it 'can create a sorted cargo list' do
+      expect(@train_yard.sorted_cargo_list).to eq(['Cattle', 'Dining', 'Mail'])
+    end
+
+    it 'can calculate total inventory' do
+      expect(@train_yard.total_inventory).to eq({@car1 => 12, @car3 => 10, @car4 => 9})
+    end
+
+    it 'can find overflow cars' do
+      expect(@train_yard.overflow_cars).to eq([@car1])
+    end
   end
 end
